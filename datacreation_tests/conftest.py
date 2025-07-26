394f9da5180a6b78f38e0eb2848b09b1af28ea16
Sabyasachi Das <avraamarnaam@gmail.com>
2025-07-26 21:55:55 +0530

    :changes

--- datacreation_tests/conftest.py
+++ datacreation_tests/conftest.py
@@ -1,10 +1,12 @@
+import os
 import pytest
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope='function')
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        is_ci = os.getenv("CI") == "true"
+        browser = p.chromium.launch(headless=is_ci)
         context = browser.new_context()
         page = context.new_page()
         yield page
